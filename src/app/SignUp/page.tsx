ec26918c6daff4f5b3a7b9458e98a434ac8b0e67
fal3n-4ngel <adiadithyakrishnan@gmail.com>
2023-09-12 23:41:14 +0530

    feat : sign up

--- src/app/SignUp/page.tsx
+++ src/app/SignUp/page.tsx
@@ -1,21 +1,27 @@
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
-export default function Login() {
+export default function SignUp() {
     return (
         <div className="flex flex-col w-full h-full">
             <section className="w-full h-full min-h-screen flex flex-col justify-center items-center">
                 <Navbar></Navbar>
                 <div className="flex flex-col justify-center items-center my-[50px] w-full ">
-                    <div className="text-[60px] font-text text-center my-5">Login</div>
+                    <div className="text-[40px] font-text text-center my-5">Create Account</div>
                     <div className="flex flex-col w-[30%] mx-auto items-center justify-center">
+
+                    <div className="flex w-[70%] text-[25px] font-extralight justify-start my-2">Name</div>
+                        <input className='flex-1 border border-black w-[70%]  p-3' id='email' type='text' aria-label='email address' />
+                        <div className="flex w-[70%] text-[25px] font-extralight justify-start my-2">Phone</div>
+                        <input className='flex-1 border border-black w-[70%]  p-3 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none' id='email' type='number' aria-label='email address' />
+
                         <div className="flex w-[70%] text-[25px] font-extralight justify-start my-2">Email</div>
                         <input className='flex-1 border border-black w-[70%]  p-3' id='email' type='email' aria-label='email address' />
                         <div className="flex w-[70%] text-[25px] font-extralight justify-start my-2">Password</div>
                         <input className=' p-3 flex-1 border border-black w-[70%]' id='email' type='password' aria-label='email address' />
-                        <div className="flex w-[70%] justify-end text-[20px] font-extralight py-2">Forgot Password</div>
-                        <div className="w-[70%] rounded-md bg-black p-3 text-white text-[20px] font-light text-center ">SIGN IN</div>
-                        <div className="font-light my-2 text-[17px]">Create Account</div>
+             
+                        <div className="w-[70%] rounded-md bg-black  text-white text-[20px] font-light text-center my-4 p-4">CREATE</div>
+                        <div className="font-light my-2 text-[17px]">Back To Sign In</div>
                     </div>
                 </div>
             
