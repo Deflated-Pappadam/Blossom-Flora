04f785f59485e0aecf6c53f7c357324c9deb97eb
Hipster16 <100425953+Hipster16@users.noreply.github.com>
2023-10-13 20:33:57 +0530

    update: package.json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import localFont from '@next/font/local'
+import localFont from "next/font/local"
 
 
 const logo = localFont({
