2a147e8971422d8c3252ee9a62efe1b3845d74d7
fal3n-4ngel <adiadithyakrishnan@gmail.com>
2023-12-02 21:20:46 +0530

    update : itemPage

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -38,8 +38,8 @@ export default function Login() {
       <section className="w-full h-full min-h-screen flex flex-col justify-center items-center">
         <Navbar></Navbar>
         <div className="flex flex-col justify-center items-center my-[50px] w-full ">
-          <div className="text-[60px] font-text text-center my-5">Login</div>
-          <div className="flex flex-col w-[30%] mx-auto items-center justify-center">
+          <div className="text-[50px] font-text text-center my-5">Login</div>
+          <div className="flex flex-col w-full md:w-[30%] mx-auto items-center justify-center ">
             <div className="flex w-[70%] text-[25px] font-extralight justify-start my-2">
               Email
             </div>
@@ -48,10 +48,15 @@ export default function Login() {
               id="email"
               type="email"
               aria-label="email address"
-              onChange={(e) => setEmail(e.target.value) }
+              onChange={(e) => setEmail(e.target.value)}
             />
-            <div className="flex w-[70%] text-[25px] font-extralight justify-start my-2">
-              Password
+            <div className="flex w-[70%] justify-between">
+              <div className="flex  text-[25px] font-extralight justify-start my-2">
+                Password
+              </div>
+              <div className="flex  justify-end text-[16px] font-extralight pt-4">
+                Forgot Password?
+              </div>
             </div>
             <input
               className=" p-3 flex-1 border border-black w-[70%]"
@@ -60,68 +65,15 @@ export default function Login() {
               aria-label="email address"
               onChange={(e) => setPassword(e.target.value)}
             />
-            <div className="flex w-[70%] justify-end text-[20px] font-extralight py-2">
-              Forgot Password
-            </div>
-            <button className="w-[70%] rounded-md bg-black p-3 text-white text-[20px] font-light text-center " onClick={handleLogin}>
+
+            <button className="w-[70%]  bg-black m-4 p-3 text-white text-[20px] font-light text-center " onClick={handleLogin}>
               SIGN IN
             </button>
             <a className="font-light my-2 text-[17px]" href="/SignUp">Create Account</a>
           </div>
         </div>
 
-        <footer className="flex flex-col w-full h-full   justify-end mt-auto">
-          <div className="flex md:flex-row flex-col justify-between mt-auto">
-            <div>
-              <div className="m-4 text-6xl text-black font-semibold">
-                Blossom Flora
-              </div>
-              <div className="m-4 text-3xl text-gray-500">
-                some great sayn by ritvik
-              </div>
-              <div className="m-4 text-2xl text-gray-500">
-                location, jksg kjf,dgdf
-              </div>
-            </div>
-            <div className="mt-10">
-              <div className="text-xl text-black font-semibold">Socials </div>
-              <a href="">
-                <div className="text-lg text-gray-500 hover:text-black cursor-pointer transition-all duration-600">
-                  twitter: ddgsd
-                </div>
-              </a>
-
-              <a href="">
-                <div className="text-lg text-gray-500 hover:text-black cursor-pointer transition-all duration-600">
-                  Insta : dsgsd
-                </div>
-              </a>
-              <a href="https://www.adithyakrishnan.com/">
-                <div className="text-lg text-gray-500 hover:text-black cursor-pointer transition-all duration-600">
-                  Website : fdhfd
-                </div>
-              </a>
-            </div>
-            <div className="mt-10">
-              <div className="text-xl text-black font-semibold">Contact Us</div>
-              <a href="">
-                <div className="text-lg text-gray-500 hover:text-black cursor-pointer transition-all duration-600">
-                  Phone:
-                </div>
-              </a>
-              <a href="">
-                <div className="text-lg text-gray-500 hover:text-black cursor-pointer transition-all duration-600">
-                  Adress 1
-                </div>
-              </a>
-              <a href="">
-                <div className="text-lg text-gray-500 hover:text-black cursor-pointer transition-all duration-600">
-                  adress 2
-                </div>
-              </a>
-            </div>
-          </div>
-        </footer>
+     
       </section>
     </div>
   );
