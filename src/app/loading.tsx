9b981750b527a4b810a977be19f9ee14f096263f
fal3n-4ngel <adiadithyakrishnan@gmail.com>
2024-02-07 19:07:35 +0530

    add : loading animation

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,62 @@
-import React from 'react'
+"use client";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+const LoadingScreen: React.FC = () => {
 
-function loading() {
-  return (
-    <div>loading</div>
-  )
-}
+  
+  const colors = ["#1b1b1b", "#1b1b1b", "#1b1b1b", "#1b1b1b", "#1b1b1b"];
 
-export default loading
+  const containerVariants = {
+    initial: {},
+    animate: {
+      transition: {
+        when: "beforeChildren",
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const dotVariants = {
+    initial: {},
+    animate: {
+      height: [40, 100, 40],
+      transition: {
+        repeat: Infinity,
+      },
+    },
+  };
+  let count = 5 ;
+
+  return <div className="w-full h-full min-h-screen flex justify-center items-center">
+    <motion.div
+      variants={containerVariants}
+      initial="initial"
+      animate="animate"
+      style={{
+        display: "flex",
+        gap: 16,
+        height: 100,
+        alignItems: "center"
+      }}
+    >
+      {Array(count)
+        .fill(null)
+        .map((_, index) => {
+          return (
+            <motion.div
+              key={index}
+              variants={dotVariants}
+              style={{
+                height: 40,
+                width: 40,
+                backgroundColor: colors[index % colors.length],
+                borderRadius: 20
+              }}
+            />
+          );
+        })}
+    </motion.div>
+  </div>;
+};
+
+export default LoadingScreen
