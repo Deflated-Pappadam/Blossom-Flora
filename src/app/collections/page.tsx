c631fdf40ac2886c0d51b1274c66a44c120a6760
Ritin George <ritingeorge.d@gmail.com>
2024-03-02 19:18:37 +0530

    rename: route names

---
+++ src/app/collections/page.tsx
@@ -0,0 +1,76 @@
+"use client"
+
+import React, { useEffect, useState } from "react";
+import CollectionItem from "@/app/components/CollectionItem";
+import Navbar from "@/app/components/Navbar";
+import { DocumentData, collection, onSnapshot } from "firebase/firestore";
+import { db } from "../../../firebase";
+import LoadingScreen from "../loading";
+
+type data = {
+  id: string
+  data: DocumentData
+}
+
+function Collections() {
+  const [allData, setAllData] = useState<data[] | null>(null);
+  const checkBox = "mr-5  sm:w-[20px] sm:h-[20px]";
+
+  useEffect(() => {
+    const unsub = onSnapshot(collection(db, "collection"), (snapshot) => {
+      setAllData(
+        snapshot.docs.map((doc) => {
+          return  {id: doc.id, data: doc.data()};
+        })
+      );
+    });
+
+    return () => unsub()
+  }, [])
+
+  if (!allData) return <LoadingScreen/>
+
+  return (
+    <div className="w-full">
+      <main className="flex w-full h-full min-h-full flex-col bg-slate-50">
+        {/* Desktop Warning for Customers Hidden in Mobile View */}
+        
+        <section className="pt-2 flex max-[1200px]:flex-col max-[1200px]:items-center bg-white relative">
+          <form className="min-[1200px]:pl-10 min-[1200px]:pr-2 w-[clamp(250px,375px,90%)]">
+            <input type="search" placeholder="Search" className="my-6 w-full border-2 border-black md:py-3 py-2 rounded-lg px-2"/>
+            <label htmlFor="filter" className="py-3 md:text-xl">
+              Collections
+            </label>
+            <ul id="filter" className="ml-2 gap-3 max-[1200px]:flex">
+              <li className="flex items-center mt-3">
+                <input
+                  id="input1"
+                  type="checkbox"
+                  className={checkBox}
+                />
+                <label htmlFor="input1" className="max-sm:text-sm">Flower</label>
+              </li>
+              <li className="flex items-center mt-3">
+                <input id="input2" type="checkbox" className={checkBox} />
+                <label htmlFor="input2" className="max-sm:text-sm">Flower</label>
+              </li>
+              <li className="flex items-center mt-3">
+                <input id="input3" type="checkbox" className={checkBox} />
+                <label htmlFor="input3" className="max-sm:text-sm">Flower</label>
+              </li>
+            </ul>
+          </form>
+        <div className="w-full mt-5 p-5 grid gap-[2.5em] md:grid-cols-[repeat(auto-fill,250px)] grid-cols-[repeat(auto-fill,200px)] md:justify-between justify-center">
+            {
+              allData.map((item) => {
+                return <CollectionItem key={item.id} ImageUrl={item.data.ImageUrl} name={item.data.Name} price={item.data.Price} id={item.id}/>
+              })
+            }
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default Collections;
---
+++ src/app/login/page.tsx
@@ -0,0 +1,87 @@
+"use client";
+;
+import { useEffect, useState } from "react";
+import { auth, getUser } from "../../../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "next/navigation";
+import toast, { Toaster } from "react-hot-toast";
+import Navbar from "../components/Navbar";
+
+export default function Login() {
+  const { push } = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    return getUser((user) => {
+      if (user) push("/");
+    });
+  });
+
+  const handleLogin = () => {
+    const user = auth.currentUser;
+    console.log(user);
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        push("/");
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        
+        toast.error(`Login Error  ${errorCode}`)
+       
+      });
+  };
+
+  return (
+    <main className="flex flex-col w-full h-full">
+      <Toaster/>
+      <Navbar />
+      <section className="w-full h-full  flex flex-col justify-center items-center ">
+        <div className="flex flex-col justify-center items-center my-[50px] w-full ">
+          <div className="text-[50px] font-text text-center my-5">Login</div>
+          <div className="flex flex-col poppins-extralight tracking-widest w-full md:w-[30%] mx-auto items-center justify-center ">
+            <div className="flex w-[90%] text-[20px] uppercase  justify-start my-2">
+              Email
+            </div>
+            <input
+              className="flex-1 border border-black w-[90%]  p-3"
+              id="email"
+              type="email"
+              aria-label="email address"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <div className="flex w-[90%] justify-between">
+              <div className="flex  text-[20px] uppercase  justify-start my-2">
+                Password
+              </div>
+              <div className="flex  justify-end text-[16px] pt-4">
+                Forgot Password?
+              </div>
+            </div>
+            <input
+              className=" p-3 flex-1 border border-black w-[90%]"
+              id="email"
+              type="password"
+              aria-label="email address"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <button
+              className="w-[90%]  bg-black m-4 p-3 text-white text-[20px] font-light text-center "
+              onClick={handleLogin}
+            >
+              SIGN IN
+            </button>
+            <a className="font-light my-2 text-[17px]" href="/SignUp">
+              Create Account
+            </a>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
